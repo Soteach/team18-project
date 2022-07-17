b4272d11d346e5594aa8b48a12f4efed55d0697c
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-07-17 23:36:03 +0300

    Зміна ширини хедер

--- src/index.js
+++ src/index.js
@@ -13,18 +13,3 @@
     });
   })();
 
-
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("hero-order__hidden");
-    }
-  })();
